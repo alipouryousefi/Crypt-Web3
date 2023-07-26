67ecd0dcfb2920248c64546b803346d08d79f260
Ali Py <a.pouryousefi98@gmail.com>
2023-07-26 13:04:16 +0330

    complete transaction section

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: 'web3',
+        replacement: 'web3/dist/web3.min.js',
+      },
+    ],
+  },
 })
