b8c6b2682b3241ceb922aff11f2fd225675169de
Ali Py <a.pouryousefi98@gmail.com>
2023-07-26 11:26:17 +0330

    create transaction contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json"
+
+export const contractABI = abi.abi
+export const contractAddress = "0xb84AA7f4B90eE6ec586Dd3E7Db07521C6EDF5985"
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,13 +1,14 @@
 // https://eth-sepolia.g.alchemy.com/v2/ryMv0qtXsqOFlXn4PD-ohxki2pwTDgxK
 
-require("@nomicfoundation/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/ryMv0qtXsqOFlXn4PD-ohxki2pwTDgxK"
+      url: "https://eth-sepolia.g.alchemy.com/v2/ryMv0qtXsqOFlXn4PD-ohxki2pwTDgxK",
+      accounts: ["825584cf4694835d43c4318cbbfcfdb00865bc819cf7c63fefe650f0d50a4f2f"]
     }
   }
 };
